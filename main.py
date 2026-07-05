f5ba9b9b7126dc2b4cfa5872fc50be0c3cb51573
Mohammad Sadegh Eftekhar <m.sadegh@tutamail.com>
2026-07-05 15:40:49 +0330

    initializing LLM models

--- main.py
+++ main.py
@@ -4,3 +4,11 @@ from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 
 load_dotenv()
+
+llm = ChatAnthropic(
+    model_name="claude-sonnet-4-5-20250929",
+    timeout=None,
+    stop=None,
+)
+# llm2 = ChatOpenAI
+
