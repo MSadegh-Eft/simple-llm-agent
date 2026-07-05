9c7ce4343174cd85d253cbac04ad3359faba433d
Mohammad Sadegh Eftekhar <m.sadegh@tutamail.com>
2026-07-05 15:50:45 +0330

    adding google gemini because of better free API

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
+from langchain_google_genai import ChatGoogleGenerativeAI
 
 load_dotenv()
 
-llm = ChatAnthropic(
+
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    # google_api_key="YOUR_API_KEY",
+)
+
+
+llm2 = ChatAnthropic(
     model_name="claude-sonnet-4-5-20250929",
     timeout=None,
     stop=None,
 )
-# llm2 = ChatOpenAI
 
+# llm3 = ChatOpenAI
