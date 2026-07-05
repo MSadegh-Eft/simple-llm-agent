7bf8e216b310762bd0bb32624b678fef31a7aecd
Mohammad Sadegh Eftekhar <m.sadegh@tutamail.com>
2026-07-05 16:20:06 +0330

    trying grok because of the 443 error

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
+import os
 from dotenv import load_dotenv
 from pydantic import BaseModel
 from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_xai import ChatXAI
 
 load_dotenv()
 
+# print(os.getenv("XAI_API_KEY")) ## checking if the API key is actually loading
 
-llm = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
-    # google_api_key="YOUR_API_KEY",
+llm = ChatXAI(
+    model = "grok-4.3"
 )
 
 
-llm2 = ChatAnthropic(
-    model_name="claude-sonnet-4-5-20250929",
-    timeout=None,
-    stop=None,
-)
+# llm2 = ChatGoogleGenerativeAI(
+#     model="gemini-2.5-flash",
+#     # google_api_key="YOUR_API_KEY",
+# )
 
-# llm3 = ChatOpenAI
+
+# llm3 = ChatAnthropic(
+#     model_name="claude-sonnet-4-5-20250929",
+#     timeout=None,
+#     stop=None,
+# )
+
+# # llm4 = ChatOpenAI
+
+response = llm.invoke("what is the meaning of life ? (short answer)")
+print(response)
