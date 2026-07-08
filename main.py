e59d15438be7997cd171a2b21c6570daf533b75f
Mohammad Sadegh Eftekhar <m.sadegh@tutamail.com>
2026-07-09 01:39:37 +0330

    adding the parser

--- main.py
+++ main.py
@@ -5,8 +5,19 @@ from langchain_openai import ChatOpenAI
 from langchain_anthropic import ChatAnthropic
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_xai import ChatXAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import PydanticOutputParser
 
 load_dotenv()
+
+
+class ReasearchResopnse(BaseModel):
+    topic: str
+    summary: str
+    sources: list[str]
+    tools_used: list[str]
+
+
 
 # print(os.getenv("XAI_API_KEY")) ## checking if the API key is actually loading
 
@@ -15,19 +26,19 @@ llm = ChatXAI(
 )
 
 
-# llm2 = ChatGoogleGenerativeAI(
-#     model="gemini-2.5-flash",
-#     # google_api_key="YOUR_API_KEY",
-# )
+llm2 = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    # google_api_key="YOUR_API_KEY",
+)
 
 
-# llm3 = ChatAnthropic(
-#     model_name="claude-sonnet-4-5-20250929",
-#     timeout=None,
-#     stop=None,
-# )
+llm3 = ChatAnthropic(
+    model_name="claude-sonnet-4-5-20250929",
+    timeout=None,
+    stop=None,
+)
 
-# # llm4 = ChatOpenAI
+# llm4 = ChatOpenAI
 
-response = llm.invoke("what is the meaning of life ? (short answer)")
-print(response)
+
+parser = PydanticOutputParser(pydantic_object=ReasearchResopnse) 
